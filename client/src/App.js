f01d33e755c68593ce0adb76e6f074e129c4383b
zushenglu <zushenglu1809@gmail.com>
2023-06-13 01:22:59 -0400

    init working post skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -26,6 +27,31 @@ function App() {
       </Router>
       <ToastContainer />
     </>
+=======
+import logo from './logo.svg';
+import './App.css';
+import Post from './post.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Post/>
+      </header>
+    </div>
+>>>>>>> 40f19cd (init working post skeleton)
   );
 }
 
