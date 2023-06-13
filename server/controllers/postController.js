313a5897e1478b98259718cfda1fdea37dff82dd
zushenglu <zushenglu1809@gmail.com>
2023-06-13 13:12:45 -0400

    connected frontend and backend of post

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,11 +1,18 @@
 import asyncHandler from "express-async-handler";
+import mongoose from "mongoose";
 
 //@route  POST api/posts
 //@desc   [DESCRIPTION OF WHAT ROUTE DOES]
 //@access [WHETHER PUBLIC OR PRIVATE i.e. LOGGED IN USER CAN ACCESS IT OR NOT]
 const createPost = asyncHandler(async (req, res) => {
-    res.send("hi there bro");
     
+    // skip authentication for now, assume id exist
+
+    // const result = JSON.parse(req.body);
+    console.log(req.body);
+    
+    res.send("hi there bro, your text is : ".concat(req.body.text));
+
 });
 
 //@route   GET api/posts
