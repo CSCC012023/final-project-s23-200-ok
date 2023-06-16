f52012f839ef2219b3e23fea4b2af30d8c9608c2
ElhamBadri2411 <elhambadri24@gmail.com>
2023-06-15 22:22:58 -0400

    merging develop into branch

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -12,16 +12,14 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-<<<<<<< HEAD
 router.route("/").post(createProfile);
 router.route("/test").get(test);
 router.route("/:id").get(getProfile);
 router.route("/:id").put(updateProfile).delete(deleteProfile);
 router.route("/:id/games/valorant").post(linkValorant);
 router.route("/:id/games/overwatch").post(linkOverwatch);
-=======
+
 router.route("/").post(protect, createProfile).get(protect, getProfile);
-router.route("/:id").put(protect, updateProfile).delete(protect, deleteProfile);
->>>>>>> develop
+// router.route("/:id").put(protect, updateProfile).delete(protect, deleteProfile);
 
 export default router;
