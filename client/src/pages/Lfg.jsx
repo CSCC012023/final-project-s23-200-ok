ff6967e8691b73ea8e9ac13ffbca7d901952007c
ElhamBadri2411 <elhambadri24@gmail.com>
2023-06-30 14:57:47 -0400

    created lfg slice and service (redux)

--- client/src/pages/Lfg.jsx
+++ client/src/pages/Lfg.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+    createPost,
+    getPosts,
+    updatePost,
+    deletePost,
+    getPost,
+    reset
+} from '../features/lfg/lfgSlice'
+import Spinner from '../components/Spinner'
 
 const Lfg = () => {
-  return (
-    <>
+    const dispatch = useDispatch();
+    const { posts, isLoading, isError, message } = useSelector((state) => state.lfg);
+    console.log(posts)
+  
+    useEffect(() => {
+      dispatch(getPosts());
+    }, [dispatch]);
+  
+    if (isLoading) {
+      return <Spinner />;
+    }
+  
+    if (isError) {
+      return <div>Error: {message}</div>;
+    }
+  
+    return (
+      <div>
         <h1>Looking For Group</h1>
         <div className='lfg-buttons'>
+          {posts.map((post) => (
+            <div key={post._id}>
+                <h3>{post.game}</h3>
+                <p>{post.notes}</p>
+            </div>
+          ))}
         </div>
-    </>
-  )
+      </div>
+    );
 }
 
 export default Lfg
