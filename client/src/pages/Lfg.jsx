4386598004f158c1f2880785b6ac8c95f3b82d1a
ElhamBadri2411 <elhambadri24@gmail.com>
2023-06-30 14:16:03 -0400

    created and implemented CRUD endpoints for lfgposts

---
+++ client/src/pages/Lfg.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Lfg = () => {
+  return (
+    <>
+        <h1>Looking For Group</h1>
+        <div className='lfg-buttons'>
+        </div>
+    </>
+  )
+}
+
+export default Lfg
