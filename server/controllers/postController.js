992e77d2d1ce63f2429364602d629dab643e11ca
ken361 <jtae1225@gmail.com>
2023-07-05 17:33:41 -0400

    Added reaction assets, small edits to postController

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import Post from "../models/Post.js";
 
 //@route  POST api/posts
 //@desc   [DESCRIPTION OF WHAT ROUTE DOES]
@@ -41,8 +42,13 @@ const deletePost = asyncHandler(async (req, res) => {
 const reactToPost = asyncHandler(async (req, res) => {
   try {
     const { reaction } = req.body; // Taking reaction type from the request body
-    const post = await Post.findById(req.params.id);
     const user = req.user.id;
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
     // Gettting the index of the reaction to update
     const reactionIndex = post.likes.findIndex(like => like.user.toString() === user);
 
@@ -61,9 +67,10 @@ const reactToPost = asyncHandler(async (req, res) => {
 
     await post.save();
     res.json(post.likes);
+    return res.status(200).json({ msg: "Reaction successful" });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error, failed to react to post');
+    return res.status(500).json({ msg: "Server error" });
   }
 });
 
