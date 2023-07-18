eaf779c7c6b265632f3f495fa17a04c5638e2479
ken361 <jtae1225@gmail.com>
2023-07-17 22:54:25 -0400

    updated front-end components for reactions
    - Added count of reactions of each type a post has, and indicator for which reaction the current user chose
    - Added styling for count of reactions
    - Slightly altered structure of a post to accommodate for reaction components (not final)
    - Removed functionality for removing reaction component when clicking outside of the component due to some unintended behavior

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,23 +1,79 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useMemo } from "react";
+import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import localeEn from "dayjs/locale/en";
 import Reactions from "./Reactions";
+import { reactionEmojis } from '../reactions/reactionEmojis';
 
 const Post = ({ post, handleDelete }) => {
   const { user } = useSelector((state) => state.auth);
-
-  const dispatch = useDispatch();
+  const [reactBtnClicked, setReactBtnClicked] = useState(false),
+        [userReaction, setUserReaction] = useState('');
 
   const calculateTime = (date) => {
     dayjs.extend(relativeTime).locale(localeEn);
     return dayjs(date).fromNow();
   };
 
-
   const handleDeletePost = () => {
     handleDelete(post._id)
+  };
+
+  const reactionCounter = useMemo(() => {
+    const reactionCount = {
+      like: 0,
+      heart: 0,
+      laugh: 0,
+      fire: 0,
+      sad: 0,
+      skull: 0
+    };
+
+    const checkUserReaction = (i) => {
+      if (post.likes[i].user.toString() === user._id) {
+        setUserReaction(post.likes[i].reaction);
+      }
+    };
+
+    for (var i = 0; i < post.likes.length; i++) {
+      var reaction = post.likes[i].reaction;
+      reactionCount[reaction]++;
+      if (userReaction === '') {
+        checkUserReaction(i);
+      }
+    }
+
+    return reactionCount;
+  }, [post, user, userReaction]);
+
+  const handleReactionButtonClick = () => {
+    if (!reactBtnClicked) {
+      setReactBtnClicked(true);
+    } else { 
+      setReactBtnClicked(false);
+    }
+  };
+
+  const generateReactionCount = () => {
+    const reactions = ['like', 'heart', 'laugh', 'fire', 'sad', 'skull'];
+
+    const checkUserReaction = (reaction) => {
+      if (reaction === userReaction) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    const reactionElements = reactions.map(reaction => (
+      <div key={reaction} className={"reaction-count-container"}>
+        <img src={reactionEmojis[reaction]} alt={reaction} />
+        <span className={`${checkUserReaction(reaction) ? 'reaction-value-hl' : 'reaction-value'}`}>{reactionCounter[reaction]}</span>
+      </div>
+    ));
+  
+    return reactionElements;
   };
 
   return (
@@ -32,10 +88,15 @@ const Post = ({ post, handleDelete }) => {
           <p>{post.text}</p>
         </div>
       </div>
-        <Reactions key={post._id} post={post} />
-      <div className="delete-post"> 
 
-      {post.user_id === user._id && (
+      <div className="reaction-count-container">
+        {generateReactionCount()}
+      </div>
+
+      <div className="post-footer"> 
+        <button className='btn' onClick={() => handleReactionButtonClick()}>React</button>
+        <Reactions key={post._id} post={post} visible={reactBtnClicked} />
+        {post.user_id === user._id && (
           // delete button
           <button className='btn' onClick={handleDeletePost}>Delete</button>
         )}
--- client/src/components/Reactions.jsx
+++ client/src/components/Reactions.jsx
@@ -1,19 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from "react-redux";
+import React, { useRef } from 'react';
+import { useDispatch } from "react-redux";
 import { reactionEmojis } from '../reactions/reactionEmojis';
 import { reactToPost } from "../features/posts/postSlice";
 
-const Reactions = ({ post }) => {
+const Reactions = ({ post, visible }) => {
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
-  const [reactBtnClicked, setReactBtnClicked] = useState(false);
   const reactionsContainerRef = useRef(null);
 
   const handleReactionClick = (reaction) => {
     dispatch(reactToPost({postId: post._id, reaction}));
-    setReactBtnClicked(false);
-  }
-
+  };
+/*
   const handleOutsideClick = (e) => {
     if (
       reactionsContainerRef.current &&
@@ -30,12 +27,11 @@ const Reactions = ({ post }) => {
       document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, []);
-
+*/
   return (
     <div>
-      <button onClick={() => setReactBtnClicked(true)}>React</button>
-      {reactBtnClicked && (
-        <div className="reactionsContainer" ref={reactionsContainerRef}>
+      {visible && (
+        <div className="reactions-container" ref={reactionsContainerRef}>
           <img src={reactionEmojis['like']} alt='like' onClick={() => handleReactionClick('like')}/>
           <img src={reactionEmojis['heart']} alt='heart' onClick={() => handleReactionClick('heart')}/>
           <img src={reactionEmojis['laugh']} alt='laugh' onClick={() => handleReactionClick('laugh')}/>
